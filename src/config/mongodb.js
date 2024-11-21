302edd0e085caa4dcb9a2846a8f27969c54efcef
masonnguyen <100501225+jasoncuong@users.noreply.github.com>
2024-11-22 00:53:40 +0700

    dotenv

---
+++ src/config/mongodb.js
@@ -0,0 +1,43 @@
+//mongodb+srv://masonnguyen:<db_password>@cluster0.jlzyf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//3BtTLpNwPJMVgv7K
+//dbxC1pZz1txTvvSU
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "~/config/environment";
+
+//Khởi tạo 1 đối tượng keeperDatabaseInstance ban đầu là null (vì chưa connect)
+let keeperDatabaseInstance = null;
+
+//Khởi tại một đối tượng mongoClientInstance để connect tới MongoDB
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  //Chỉ định 1 cái Stable API Version của MongoDB
+  //https://www.mongodb.com/docs/drivers/node/current/fundamentals/stable-api/
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//Kết nối tới Database
+export const CONNECT_DB = async () => {
+  //Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect();
+
+  //Kết nối thành công thì lấy ra Database theo tên và gán ngược lại vào biến keeperDatabaseInstance
+  keeperDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
+};
+
+//Đóng kết nối tới Database khi cần
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+};
+
+//Functuon GET_DB (không async) có nhiệm vụ export ra cái keeperDatabaseInstance sau khi đã connect thành công
+//tới MongoDB để sử dụng ở nhiều nơi khác nhau trong code
+//Chỉ gọi cái GET_DB này khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!keeperDatabaseInstance)
+    throw new Error("Must connect to Database first");
+  return keeperDatabaseInstance;
+};
--- src/server.js
+++ src/server.js
@@ -1,15 +1,49 @@
-// const express = require("express");
 import express from "express";
+import exitHook from "async-exit-hook";
+import { CONNECT_DB, CLOSE_DB } from "~/config/mongodb";
+import { env } from "~/config/environment";
+const START_SERVER = () => {
+  const app = express();
 
-const app = express();
+  app.get("/", async (req, res) => {
+    res.end("<h1>Hello Mason!</h1><hr>");
+  });
 
-const hostname = "localhost";
-const port = "8017";
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(
+      `Hi ${env.AUTHOR}, Back-end Server is running successfully at http://${env.APP_PORT}:${env.APP_HOST}/`
+    );
+  });
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World Mason Nguyen</h1>");
-});
+  //Thực hiện các thao tác cleanup trước khi dừng server lại
+  exitHook(() => {
+    console.log("Server is shuting down...");
+    CLOSE_DB();
+    console.log("Disconnected from MongoDB Cloud Atlas");
+  });
+};
 
-app.listen(port, hostname, () => {
-  console.log(`Hello Mason, I'm running server at http://${hostname}:${port}`);
-});
+//Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên
+//Immediately Invoked Function Expression (IIFE)
+(async () => {
+  try {
+    console.log("Connecting to MongoDB Cloud Atlas...");
+    await CONNECT_DB();
+    console.log("Connected to MongoDB Cloud Atlas");
+
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
+
+//Cách viết khác
+//Chỉ khi kết nối tới Database thành công thì mới Start Server Back-end lên
+// CONNECT_DB()
+//   .then(() => console.log("Connected to MongoDB Cloud Atlas"))
+//   .then(() => START_SERVER())
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(0);
+//   });
