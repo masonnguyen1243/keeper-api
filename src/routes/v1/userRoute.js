fd2bce2818de98e16d44a2bd232efbe5b2bc3b63
masonnguyen1243 <meowjcisgod@gmail.com>
2025-05-05 16:20:19 +0700

    Logout

--- src/routes/v1/userRoute.js
+++ src/routes/v1/userRoute.js
@@ -16,4 +16,8 @@ Router.route("/verify").post(
 
 Router.route("/login").post(userValidation.login, userController.login);
 
+Router.route("/logout").delete(userController.logout);
+
+Router.route("/refresh_token").get(userController.refreshToken);
+
 export const userRoute = Router;
