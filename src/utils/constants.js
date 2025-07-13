9febec81c3003fe2e4624e3b72b83e7f6e300a8c
masonnguyen1243 <meowjcisgod@gmail.com>
2025-07-13 15:17:48 +0700

    Update cors not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,9 @@
 import { env } from "~/config/environment";
 
 //Domain được phép truy cập vào dữ liệu tài nguyên của server
-export const WHITELIST_DOMAINS = ["http://localhost:5173"];
+export const WHITELIST_DOMAINS = [
+  // "http://localhost:5173"
+];
 
 export const BOARD_TYPE = {
   PUBLIC: "public",
